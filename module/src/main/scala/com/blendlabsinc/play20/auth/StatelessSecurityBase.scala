cced85db1741c60f8d277b2a752afd3a9e84052a
Quinn Slack <qslack@qslack.com>
2012-05-24 16:37:26 -0700

    allow differentially typed BodyParsers

--- module/src/main/scala/com/blendlabsinc/play20/auth/StatelessSecurityBase.scala
+++ module/src/main/scala/com/blendlabsinc/play20/auth/StatelessSecurityBase.scala
@@ -6,7 +6,7 @@ trait StatelessSecurityBase {
 
   type User
 
-  def userId(request: RequestHeader): Option[String] =
+  def getUserIdFromRequest(request: RequestHeader): Option[String] =
     AuthData.decodeFromCookie(request.cookies.get(AuthData.COOKIE_NAME)).get(AuthData.UserIdKey)
 
   def lookupUser(userId: String): Option[User]
@@ -16,22 +16,34 @@ trait StatelessSecurityBase {
   def onLoginSucceeded(request: RequestHeader): PlainResult
   def onLogoutSucceeded(request: RequestHeader): PlainResult
 
-  def withAuth(f: => String => Request[AnyContent] => Result) = {
-    Security.Authenticated(userId, onUnauthorized) { user =>
-      Action(request => f(user)(request))
-    }
-  }
+  def withAuth(f: => String => Request[AnyContent] => Result): Action[AnyContent] =
+    withAuth(BodyParsers.parse.anyContent)(f)
 
-  def withUser(f: User => Request[AnyContent] => Result) = withAuth { userId => implicit request =>
+  def withAuth[A](p: BodyParser[A])(f: => String => Request[A] => Result): Action[A] =
+    Action(p)(request => getUserIdFromRequest(request) match {
+      case Some(userId) => f(userId)(request)
+      case None => onUnauthorized(request)
+    })
+
+  def withUser(f: User => Request[AnyContent] => Result): Action[AnyContent] =
+    withUser(BodyParsers.parse.anyContent)(f)
+
+  def withUser[A](p: BodyParser[A])(f: User => Request[A] => Result): Action[A] = withAuth(p) { userId => implicit request =>
     lookupUser(userId).map { user =>
       f(user)(request)
     }.getOrElse(onUnauthorized(request))
   }
 
-  def withOptionalAuth(f: Option[String] => Request[AnyContent] => Result) =
-    Action(request => f(userId(request))(request))
+  def withOptionalAuth(f: Option[String] => Request[AnyContent] => Result): Action[AnyContent] =
+    withOptionalAuth(BodyParsers.parse.anyContent)(f)
 
-  def withOptionalUser(f: Option[User] => Request[AnyContent] => Result) = withOptionalAuth { userId => implicit request =>
+  def withOptionalAuth[A](p: BodyParser[A])(f: Option[String] => Request[A] => Result): Action[A] =
+    Action(p)(request => f(getUserIdFromRequest(request))(request))
+
+  def withOptionalUser(f: Option[User] => Request[AnyContent] => Result): Action[AnyContent] =
+    withOptionalUser(BodyParsers.parse.anyContent)(f)
+
+  def withOptionalUser[A](p: BodyParser[A])(f: Option[User] => Request[A] => Result): Action[A] = withOptionalAuth(p) { userId => implicit request =>
     userId.map { userId =>
       lookupUser(userId).map { user =>
         f(Some(user))(request)
